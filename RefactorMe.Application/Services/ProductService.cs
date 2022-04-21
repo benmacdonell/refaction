5a51f77b4eed39ae458104f6b56bcbd4d130a5f5
Pete <pete.russell01@gmail.com>
2022-04-22 10:30:42 +1200

    Added: test examples.

--- RefactorMe.Application/Services/ProductService.cs
+++ RefactorMe.Application/Services/ProductService.cs
@@ -11,7 +11,7 @@ namespace RefactorMe.Application.Services
 
         public ProductService(IProductRepository repository)
         {
-            this._repository = repository;
+            _repository = repository;
         }
 
         public IEnumerable<Guid> GetProducts() {
@@ -20,51 +20,109 @@ namespace RefactorMe.Application.Services
 
         public IEnumerable<Guid> GetProducts(string name)
         {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Product name not specified");
+            }
+
             return _repository.GetProducts(name);
         }
 
         public Product GetProduct(Guid id)
         {
+            if (id == null)
+            {
+                throw new ArgumentException("Product ID must not be null");
+            }
+
             return _repository.GetProduct(id);
         }
 
         public void CreateProduct(Product product)
         {
+            if (product == null)
+            {
+                throw new ArgumentException("Product must not be null");
+            }
+
             _repository.InsertProduct(product);
         }
 
         public void UpdateProduct(Guid id, Product product)
         {
+            if (id == null)
+            {
+                throw new ArgumentException("Product ID must not be null");
+            }
+            if (product == null)
+            {
+                throw new ArgumentException("Product must not be null");
+            }
+
             _repository.UpdateProduct(id, product);
         }
 
         public void DeleteProduct(Guid id)
         {
+            if (id == null)
+            {
+                throw new ArgumentException("Product ID must not be null");
+            }
             _repository.DeleteProduct(id);
         }
 
         public IEnumerable<ProductOption> GetOptions(Guid productId)
         {
+            if (productId == null)
+            {
+                throw new ArgumentException("Product ID must not be null");
+            }
+
             return _repository.GetOptions(productId);
         }
 
         public ProductOption GetOption(Guid id)
         {
+            if (id == null)
+            {
+                throw new ArgumentException("Product option ID must not be null");
+            }
+
             return _repository.GetOption(id);
         }
 
         public void CreateOption(Guid productId, ProductOption option)
         {
+            if (productId == null)
+            {
+                throw new ArgumentException("Product ID must not be null");
+            }
+            if (option == null)
+            {
+                throw new ArgumentException("Product option must not be null");
+            }
             _repository.InsertOption(productId, option);
         }
 
         public void UpdateOption(Guid id, ProductOption option)
         {
+            if (id == null)
+            {
+                throw new ArgumentException("Product option ID must not be null");
+            }
+            if (option == null)
+            {
+                throw new ArgumentException("Product option must not be null");
+            }
             _repository.UpdateOption(id, option);
         }
 
         public void DeleteOption(Guid id)
         {
+            if (id == null)
+            {
+                throw new ArgumentException("Product option ID must not be null");
+            }
             _repository.DeleteOption(id);
         }
     }
---
+++ RefactorMe.Tests/Application/Services/ProductServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using Moq;
+using RefactorMe.Application.Services;
+using RefactorMe.Domain.Entities;
+using RefactorMe.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace RefactorMe.Tests.Application.Services
+{
+    public class ProductServiceTests
+    {
+        Mock<IProductRepository> _mockRepo;
+        ProductService _sut;
+
+        public ProductServiceTests()
+        {
+            _mockRepo = new Mock<IProductRepository>();
+            _sut = new ProductService(_mockRepo.Object);
+        }
+
+        [Fact]
+        public void GetProductsShouldReturnListOfGuids()
+        {
+            // Arrange
+            var guids = new List<Guid>
+            {
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid()
+            };
+
+            _mockRepo.Setup(p => p.GetProducts()).Returns(guids);
+
+            // Act
+            var res = _sut.GetProducts().ToList();
+
+            // Assert
+            Assert.Equal(3, res.Count);
+            Assert.Equal(res[0], guids[0]);
+            Assert.Equal(res[1], guids[1]);
+            Assert.Equal(res[2], guids[2]);
+        }
+
+        [Fact]
+        public void GetProductsShouldReturnEmptyListIfNoProductsFound()
+        {
+            // Arrange
+            _mockRepo.Setup(p => p.GetProducts()).Returns(new List<Guid>());
+
+            // Act
+            var res = _sut.GetProducts().ToList();
+
+            // Assert
+            Assert.NotNull(res);
+            Assert.Empty(res);
+        }
+
+        [Fact]
+        public void CreateProductShouldSaveProductToRepository()
+        {
+            // Arrange
+            var product = new Product
+            {
+                Name = "My Product",
+                Description = "This is my first product",
+                Price = 24.99M,
+                DeliveryPrice = 2.99M
+            };
+
+            // Act
+            _sut.CreateProduct(product);
+
+            // Assert
+            _mockRepo.Verify(m => m.InsertProduct(product), Times.Once());
+        }
+
+        // Plus lots more tests...
+    }
+}
